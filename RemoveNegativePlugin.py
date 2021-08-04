f9069662d829fe3843e37d4c0dd0556abba15e39
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-04 17:22:54 -0400

    Changed this to remove negative edges, which is really what it should do
    It was formerly doing absolute values

--- RemoveNegativePlugin.py
+++ RemoveNegativePlugin.py
@@ -18,8 +18,22 @@ class RemoveNegativePlugin(CSV2GMLPlugin):
          else:
             filestuff.write(self.bacteria[i]+",")
          for j in range(self.n-1):
-            filestuff.write(str(abs(self.ADJ[i][j]))+",")
-         filestuff.write(str(abs(self.ADJ[i][self.n-1])))
+             if (i == j): #CSV2GML Removes This
+               filestuff.write("1,")
+             else:
+               if (self.ADJ[i][j] < 0):
+                   filestuff.write("0,")
+               else:
+                   filestuff.write(str(self.ADJ[i][j])+",")
+               #filestuff.write(str(abs(self.ADJ[i][j]))+",")
+         if (i != self.n-1):
+            if (self.ADJ[i][j] < 0):
+                filestuff.write("0")
+            else:
+                filestuff.write(str(self.ADJ[i][self.n-1]))
+            #filestuff.write(str(abs(self.ADJ[i][self.n-1])))
+         else:
+            filestuff.write("1")
          filestuff.write("\n")
 
 
